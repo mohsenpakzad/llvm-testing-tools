992c9104d858494ef905d8950142633cb847b9f5
Mohsen Pakzad <mohsen137853@gmail.com>
2022-05-30 20:00:26 +0430

    Add phase 2 files

---
+++ Phase 2 – Fuzz Testing on LLVM IR/cr.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# compile and run
+
+clang++-10  -o FuzzTester FuzzTester.cpp `llvm-config-10 --cxxflags` `llvm-config-10 --ldflags` `llvm-config-10 --libs` -lpthread -lncurses -ldl
+ ./FuzzTester sample-codes/test1.ll
